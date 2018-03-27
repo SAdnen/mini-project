b26a2c56d54a17d86ed31e80909e3b949a39c547
SAdnen <saadaoui.adnen@gmail.com>
2018-03-28 00:10:01 +0100

    edit main and cartpole

--- cartpole.py
+++ cartpole.py
@@ -7,28 +7,32 @@ class CartPole(object):
     This Class defines the defaul behavior of CartPole agent
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, action_space):
+        self.action_space = action_space
 
-    def act(self, action_space):
+    def act(self):
         """ Returns random action from action space"""
         return self.action_space.sample()
+
+    def learn(self):
+        raise NotImplementedError
 
 
 class RandomSearch(CartPole):
 
-    def __init__(self, reward_tresh=50, parameters=None):
-        super().__init__()
+    def __init__(self, action_space, reward_tresh=50, parameters=None):
+        super().__init__(action_space)
         if parameters is None:
             self.reward_tresh = reward_tresh
             self.parameters = np.random.random(4) * 2 - 1
             self.best_parameters = []
         else:
-            assert len(parameters) == 4, "length parameters should be 4"
+            assert len(
+                parameters) == 4, "parameters should be an array of length 4"
             self.parameters = parameters
 
     def act(self, state):
-        assert len(state) == 4, "length of state for cartople shoud be 4"
+        assert len(state) == 4, "state should be an array of leng 4"
         action = np.dot(self.parameters, state)
 
         if action > 0:
@@ -37,7 +41,8 @@ class RandomSearch(CartPole):
             return 0
 
     def learn(self, reward, done):
-        if reward > self.reward_tresh:
+        if reward >= self.reward_tresh:
+            self.reward_tresh = reward
             self.best_parameters.append((self.parameters, reward))
         if done:
             self.parameters = np.random.random(4) * 2 - 1
--- main.py
+++ main.py
@@ -9,12 +9,12 @@ def main():
     INTERACTIVE = False
     env_name = "CartPole-v0"
     env = gym.make(env_name)
-    agent = cartpole.RandomSearch(reward_tresh=50, parameters=None)
+    action_space = env.action_space
+    agent = cartpole.RandomSearch(
+        action_space, reward_tresh=50, parameters=None)
     experiment = Experiment(env, agent)
     experiment.run_randomsearch(
         max_number_of_episodes=50, interactive=INTERACTIVE)
-    [print("(Best parameters, reward)[{}]: {}".format(i, p))
-     for i, p in enumerate(agent.best_parameters)]
 
 
 if __name__ == '__main__':
