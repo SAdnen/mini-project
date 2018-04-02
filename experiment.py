7cd217dc7ed7b984f359f7cff9eac9e7c61c30d9
marwensallem <sallem.marwen@yahoo.fr>
2018-04-02 23:38:14 +0200

    edit experiment

--- experiment.py
+++ experiment.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from time import sleep
 
+
+
 def plot_graphs(episode_reward, episode_length):
     episode_lengths = episode_length
     episode_rewards = episode_reward
@@ -86,6 +88,42 @@ class Experiment(object):
             self.env.close()
 
         plot_graphs(self.episode_reward, self.episode_length)
+
+    def run_randomsearch(self, max_number_of_episodes=100, interactive=False,
+                         display_frequency=1):
+        # repeat for each episode
+        for episode in range(max_number_of_episodes):
+            state = self.env.reset()  # Initialization
+            done = False  # used to indicate terminal state
+            R = 0  # used to display accumulated rewards for an episode
+            t = 0  # used to display accumulated steps for an episode
+            # repeat for each step of episode, until state is terminal
+            while not done:
+
+                t += 1  # increase step counter - for display
+
+                # choose action from state using policy derived from Q
+                action = self.agent.act(state)
+
+                # take action, observe reward and next state
+                state, reward, done, _ = self.env.step(action)
+
+                R += reward  # accumulate reward - for display
+
+                # if interactive display, show update for each step
+                if interactive:
+                    self.env.render()
+            self.agent.learn(R, done)
+
+            self.episode_length = np.append(
+                self.episode_length, t)  # keep episode length - for display
+            self.episode_reward = np.append(
+                self.episode_reward, R)  # keep episode reward - for display
+
+            # if interactive display, show update for the episode
+        if interactive:
+            self.env.render()
+            self.env.close()
 
 
     def run_qlearning(self, max_number_of_episodes=100,
