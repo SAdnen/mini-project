d1b15edd2ae865a7577353fac163b549c525df15
SAdnen <saadaoui.adnen@gmail.com>
2018-04-04 11:07:54 +0100

    changed the import call for the agent

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 # ! /usr/bin/env python3
 
 
-from cartpole import CartPole, QlearningAgent
-import cartpole
+from agents.qlearning import QlearningAgent
 from experiment import Experiment
 import gym
 
 
-
 def main():
     interactive = False
-    debug=True
+    debug = True
     env_name = "CartPole-v0"
     env = gym.make(env_name)
     action_space = env.action_space
@@ -18,13 +16,10 @@ def main():
                            high=env.observation_space.high,
                            low=env.observation_space.low)
 
-
-
     experiment = Experiment(env, agent)
-    experiment.run_qlearning(max_number_of_episodes=500, interactive=interactive, debug=debug)
+    experiment.run_qlearning(
+        max_number_of_episodes=500, interactive=interactive, debug=debug)
 
 
 if __name__ == '__main__':
     main()
-
-
